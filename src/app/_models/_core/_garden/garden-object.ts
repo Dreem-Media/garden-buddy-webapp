a3571165c1fff4c6b0f3c515360871bc37b2a5a2
Alexander Whitlock <zanderwhitlock@gmail.com>
2022-07-30 09:12:09 +0300

    template updates

--- src/app/_models/_core/_garden/garden-object.ts
+++ src/app/_models/_core/_garden/garden-object.ts
@@ -1,5 +1,6 @@
 import { API_GardenObject } from 'src/app/api/models';
 export class GardenObject implements API_GardenObject {
+  title?: string;
   description?: string | undefined;
   garden_category_ids?: string[] | undefined;
   garden_object_tasks?: string[] | undefined;
@@ -7,6 +8,7 @@ export class GardenObject implements API_GardenObject {
   image?: string | undefined;
   name: string;
   owned_count?: number | undefined;
+  gardenTasks?: any[];
 
   constructor(gardenObject?: GardenObject) {
     this.name = gardenObject?.name ?? '';
